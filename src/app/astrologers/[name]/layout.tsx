9ba7c4a2e16c6b9d8336cb3c8d72a371d8fc14a2
Raju Bharali <pushtakgharofficial@gmail.com>
2025-07-10 08:10:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <MobileNav>
    
    with

---
+++ src/app/astrologers/[name]/layout.tsx
@@ -0,0 +1,10 @@
+// This is a new file created by App Prototyper to override the root layout
+// and remove the main MobileNav from the astrologer profile page.
+
+export default function AstrologerProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
